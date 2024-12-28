db161f0dcaf3e2b687f3f0f1cff19620dfa0e2e0
Pramish-a11y <63737910+Pramish-a11y@users.noreply.github.com>
2024-12-29 00:13:23 +0400

    Add main landing page

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+import ServiceSection from '../components/ServiceSection';
+import CTASection from '../components/CTASection';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeroSection />
+      <ServiceSection />
+      <CTASection />
+    </div>
+  );
+};
+
+export default LandingPage;
